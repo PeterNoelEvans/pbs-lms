28bbc0e77612fe47f62db5097c2295acfaeb9f1c
Peter Evans <sjchpb@gmail.com>
2025-03-10 10:36:41 +0700

    Fix cookies

--- server.js
+++ server.js
@@ -194,7 +194,6 @@ async function initializeApp() {
                 secure: true,
                 httpOnly: true,
                 sameSite: 'none',
-                domain: '.onrender.com',
                 path: '/',
                 maxAge: 24 * 60 * 60 * 1000 // 24 hours
             }
